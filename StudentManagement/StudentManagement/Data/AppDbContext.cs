542176057ebf4a6880eeafba37d0e1c883899288
ArunVangala <vangalaarun74@gmail.com>
2025-09-17 11:11:13 +0530

    updated the studentmangement folder

---
+++ StudentManagement/StudentManagement/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+
+namespace StudentManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected AppDbContext()
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+    }
+
+}
+
---
+++ StudentManagement/StudentManagement/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Range(1, 100)]
+        public int Age { get; set; }
+
+        [StringLength(100)]
+        public string Course { get; set; }
+
+        [Range(0, 100)]
+        public decimal? Marks { get; set; }
+    }
+}
